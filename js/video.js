a98f294df78d19fe83d79491c39a736153707bd6
mattrm0830 <114193491+mattrm0830@users.noreply.github.com>
2023-11-08 09:24:03 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,64 @@
+var video = document.querySelector('#player1');
+
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window");
+	video.autoplay = false;
+	video.pause();
+	video.loop = false;
+	document.querySelector("#vintage").style.display = "none";
+
+}); 
+document.querySelector("#play").addEventListener("click", function (){
+		video.play();
+		document.querySelector("#volume").innerHTML = document.querySelector("#slider").value + "%";
+		
+	
+});
+document.querySelector("#pause").addEventListener("click", function (){
+		video.pause();
+});
+document.querySelector('#slower').addEventListener("click", function() {
+	slowerRate = video.playbackRate/10;
+	video.playbackRate = video.playbackRate - slowerRate;
+	console.log(video.playbackRate);
+});
+document.querySelector('#faster').addEventListener("click", function() {
+	slowerRate = video.playbackRate/10;
+	video.playbackRate = video.playbackRate + slowerRate;
+	console.log(video.playbackRate);
+});
+document.querySelector('#skip').addEventListener("click", function() {
+	var newTime = video.currentTime + 10
+	if(newTime >= video.duration) {
+		video.currentTime = 0;
+	}
+	else{
+		video.currentTime = newTime;
+	}
+	console.log(video.currentTime);
+});
+document.querySelector('#mute').addEventListener("click", function (){
+	if(video.muted==false){
+		video.muted=true;
+		document.querySelector('#mute').innerHTML = "Unmute"
+	}
+	else{
+		document.querySelector('#mute').innerHTML = "Mute"
+		video.muted = false;
+	}
+	
+});
+document.querySelector('#slider').addEventListener("click", function (){
+	video.volume = document.querySelector('#slider').value/100;
+	document.querySelector("#volume").innerHTML = document.querySelector("#slider").value + "%";
+});
+
+
+
+
+
+// document.querySelector("#play").addEventListener("click", function() {
+// 	console.log("Play Video");
+// });
+
